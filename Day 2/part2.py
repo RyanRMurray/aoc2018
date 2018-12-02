5b0b0ea602e238125b6033c3295f3d669a83c02d
RyanRMurray@Hotmail.co.uk <RyanRMurray@Hotmail.co.uk>
2018-12-02 10:03:13 +0000

    end of day 2

---
+++ Day 2/part2.py
@@ -0,0 +1,33 @@
+import os
+import string
+import operator
+
+strings = list()
+found = False
+input = open("input.txt", "r")
+
+
+for line in input:
+	strings.append(line.rstrip())
+
+for firstStr in strings:
+	for secondStr in strings:
+		diff = 0
+		list1 = list(firstStr)
+		list2 = list(secondStr)
+		i = 0
+		for first, second in zip(list1, list2):
+			if first != second:
+				diff += 1
+				pos = i
+			i += 1
+			
+		if diff == 1:
+			found = True
+			out = firstStr
+			break
+	if found: break
+
+print(out[:pos] + out[pos+1:])
+
+
