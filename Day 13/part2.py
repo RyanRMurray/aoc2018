a7d49d192fbf271445cad58a15439d31634d7665
RyanRMurray@Hotmail.co.uk <RyanRMurray@Hotmail.co.uk>
2018-12-15 15:13:56 +0000

    End of Day 13

--- Day 13/part2.py
+++ Day 13/part2.py
@@ -1,5 +1,5 @@
 import os
-import operator
+import copy
 from enum import Enum
 
 #directions of train - north, east, south, west
@@ -19,7 +19,8 @@ class train_obj:
 		self.x = x
 		self.y = y
 		self.facing = facing
-		self.inter = inter
+		self.inter = inter #defines intersection behaviour
+		self.crashed = False
 
 def print_track(grid, trains):
 	out = open('out.txt','a')
@@ -28,7 +29,7 @@ def print_track(grid, trains):
 		for x in range(len(grid[y])):
 			for t in trains:
 				train = False
-				if t.x == x and t.y == y:
+				if t.x == x and t.y == y and not t.crashed:
 					line += 't'
 					train = True
 					break
@@ -49,107 +50,117 @@ def print_track(grid, trains):
 	out.write('\n')
 	out.close()
 
-#get tracks and trains from input
-grid = []
-trains = []
-with open('test.txt', 'r') as input:
-	y = 0
-	for line in input:
-		row = []
-		x = 0
-		for char in line:
-			#rails
-			if char == ' ':
-				row.append(r.NONE)
-			elif char == '-':
-				row.append(r.WE)
-			elif char == '|':
-				row.append(r.NS)
-			elif char == '/':
-				row.append(r.NW)
-			elif char == '\\':
-				row.append(r.NE)
-			elif char == '+':
-				row.append(r.CROSS)
-			#trains
-			elif char == '^':
-				trains.append(train_obj(x,y,0,0))
-				row.append(r.NS)
-			elif char == '>':
-				trains.append(train_obj(x,y,1,0))
-				row.append(r.WE)
-			elif char == 'v':
-				trains.append(train_obj(x,y,2,0))
-				row.append(r.NS)
-			elif char == '<':
-				trains.append(train_obj(x,y,3,0))
-				row.append(r.WE)
-			x += 1
-		grid.append(row)
-		y += 1
-
-print('Number of trains: ', len(trains))
-trains.sort(key = lambda l:(l.y, l.x))
-#get current locations
-positions = []
-for t in trains:
-	positions.append((t.x, t.y))
-while True:
-	print_track(grid, trains)
-	trains.sort(key = lambda l:(l.y, l.x))
-	t = 0
-	while t < len(trains):
-		rail = grid[trains[t].y][trains[t].x]
-		#track behaviour
-		if rail == r.NE: # \
-			if trains[t].facing == 0:
-				trains[t].facing = 3
-			elif trains[t].facing == 1:
-				trains[t].facing = 2
-			elif trains[t].facing == 2:
-				trains[t].facing = 1
-			else:
-				trains[t].facing = 0
-		elif rail == r.NW: # /
-			if trains[t].facing == 0:
-				trains[t].facing = 1
-			elif trains[t].facing == 1:
-				trains[t].facing = 0
-			elif trains[t].facing == 2:
-				trains[t].facing = 3
-			else:
-				trains[t].facing = 2
-		elif rail == r.CROSS:
-			if trains[t].inter == 0:
-				if trains[t].facing == 0:
-					trains[t].facing = 3
-				else:
-					trains[t].facing -= 1
-				trains[t].inter += 1
-			elif trains[t].inter == 1:
-				trains[t].inter += 1
-			else:
-				if trains[t].facing == 3:
-					trains[t].facing = 0
-				else:
-					trains[t].facing += 1
-				trains[t].inter = 0
-		trains[t].x += direction[trains[t].facing][0]
-		trains[t].y += direction[trains[t].facing][1]
-		new_pos = (trains[t].x, trains[t].y)
-		#check for collisions
-		if new_pos in positions:
-			other = positions.index(new_pos)
-			del trains[t]
-			del trains[other]
-			del positions[t]
-			del positions[other]
+def move_train(rail, train):
+	#track behaviour
+	if rail == r.NE: # \
+		if train.facing == 0:
+			train.facing = 3
+		elif train.facing == 1:
+			train.facing = 2
+		elif train.facing == 2:
+			train.facing = 1
 		else:
-			print(positions)
-			print(t)
-			positions[t] = new_pos
-			t += 1
-	if len(trains) == 1:
-		break
-print('Last remaining train found at ', trains[0].x, trains[0].y)
+			train.facing = 0
+	elif rail == r.NW: # /
+		if train.facing == 0:
+			train.facing = 1
+		elif train.facing == 1:
+			train.facing = 0
+		elif train.facing == 2:
+			train.facing = 3
+		else:
+			train.facing = 2
+	elif rail == r.CROSS:
+		if train.inter == 0:
+			if train.facing == 0:
+				train.facing = 3
+			else:
+				train.facing -= 1
+			train.inter += 1
+		elif train.inter == 1:
+			train.inter += 1
+		else:
+			if train.facing == 3:
+				train.facing = 0
+			else:
+				train.facing += 1
+			train.inter = 0
+	train.x += direction[train.facing][0]
+	train.y += direction[train.facing][1]
+
+def get_input():
+	grid = []
+	trains = []
+	with open('input.txt', 'r') as input:
+		y = 0
+		for line in input:
+			row = []
+			x = 0
+			for char in line:
+				#rails
+				if char == ' ':
+					row.append(r.NONE)
+				elif char == '-':
+					row.append(r.WE)
+				elif char == '|':
+					row.append(r.NS)
+				elif char == '/':
+					row.append(r.NW)
+				elif char == '\\':
+					row.append(r.NE)
+				elif char == '+':
+					row.append(r.CROSS)
+				#trains
+				elif char == '^':
+					trains.append(train_obj(x,y,0,0))
+					row.append(r.NS)
+				elif char == '>':
+					trains.append(train_obj(x,y,1,0))
+					row.append(r.WE)
+				elif char == 'v':
+					trains.append(train_obj(x,y,2,0))
+					row.append(r.NS)
+				elif char == '<':
+					trains.append(train_obj(x,y,3,0))
+					row.append(r.WE)
+				x += 1
+			grid.append(row)
+			y += 1
+		return [grid, trains]
+
+def main() :
+	input = get_input()
+	grid = input[0]
+	trains = input[1]
+	print('Number of trains: ', len(trains))
+	trains.sort(key = lambda l:(l.y, l.x))
+	crashed = 0
+
+
+	while crashed < len(trains) - 1:
+		#ensure trains are considered left to right, top to bottom.
+		trains.sort(key = lambda l:(l.y, l.x))
+		
+		for t in trains:
+			if not t.crashed:
+				#get the rail the train is on
+				rail = grid[t.y][t.x]
+				#first, move the train
+				move_train(rail, t)
+				#then see if it has collided with another train
+				for other_t in trains:
+					if t != other_t and not other_t.crashed:
+						if t.x == other_t.x and t.y == other_t.y:
+							print('Collision at ', t.x, t.y)
+							t.crashed = True
+							other_t.crashed = True
+							crashed += 2
+		#for bug testing:
+		#print_track(grid, trains)
+
+	for t in trains:
+		if not t.crashed:
+			print('Last train found at ', t.x, t.y)
+
+main()
 
