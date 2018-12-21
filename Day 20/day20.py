84d537fb57f5c1a5e2ad4d8524f7e66bb4bbe785
RyanRMurray@Hotmail.co.uk <RyanRMurray@Hotmail.co.uk>
2018-12-21 09:07:27 +0000

    End of Day 20

--- Day 20/day20.py
+++ Day 20/day20.py
@@ -6,96 +6,72 @@ dir = {
 	'W' : [-1,0],
 	'S' : [0, 1]
 }
-cycles = [
-	'NEWS', 'NESW', 'NSEW', 'SNEW', 'SNWE', 'SWNE', 'WSNE', 'WSEN', 'WESN', 'EWSN', 'EWNS', 'ENWS'
-]
-grid = {} #(x,y) -> (0: open space, 1: NS door, 2: WE door)
+grid = {} #point -> 0 for open space, 1 for door
 
 def get_input(f_path):
 	input = open(f_path, 'r').read()
-	finished = False
-	
-	while not finished:
-		before = input
-		for c in cycles:
-			input = input.replace(c, '')
-		if before == input:
-			finished = True
 	return list(input)
 
-def branch_gen(ptr, input, dist):
+def build_branch(string, ptr):
 	global grid
-	start_ptr = ptr[:]
-	start_dist = dist
+	root = ptr
 	
-	grid[(ptr[0],ptr[1])] = (0,dist)
-	while len(input) > 0:
-		char = input.pop(0)
-		if char in ['N','W','E','S']:
-			ptr[0] += dir[char][0]
-			ptr[1] += dir[char][1]
-			dist += 1
-			if char in ['N','S']:
-				if (ptr[0],ptr[1]) not in grid:
-					grid[(ptr[0],ptr[1])] = (1,dist)
-			else:
-				if (ptr[0],ptr[1]) not in grid:
-					grid[(ptr[0],ptr[1])] = (2,dist)
-			ptr[0] += dir[char][0]
-			ptr[1] += dir[char][1]
-			if (ptr[0],ptr[1]) not in grid:
-				grid[(ptr[0],ptr[1])] = (0,dist)
+	while len(string) > 0:
+		char = string.pop(0)
+		if char in 'NEWS':
+			d = dir[char]
+			door = (ptr[0]+d[0],ptr[1]+d[1])
+			next = (door[0]+d[0],door[1]+d[1])
+			grid[door] = 1
+			grid[next] = 0
+			ptr = next
+		elif char == '|':
+			ptr = root
 		elif char == '(':
 			depth = 1
-			sub_branch = []
-			while depth != 0:
-				char = input.pop(0)
+			sub_branch = ''
+			while depth > 0:
+				char = string.pop(0)
 				if char == '(':
 					depth += 1
 				elif char == ')':
 					depth -= 1
-				sub_branch.append(char)
-			branch_gen(ptr, sub_branch, dist)
-		elif char == '|':
-			ptr = start_ptr
-			dist = start_dist
+				sub_branch += char
+			build_branch(list(sub_branch[0:-1]), ptr)
 
-def print_grid():
-	out = open('out.txt', 'a')
-	min_y = min(grid.keys(), key=itemgetter(1))[1] - 1
-	min_x = min(grid.keys(), key=itemgetter(0))[0] - 1
-	max_y = max(grid.keys(), key=itemgetter(1))[1] + 1
-	max_x = max(grid.keys(), key=itemgetter(0))[0] + 1
+def bfs(start):
+	path_grid = {}
+	to_visit = [start]
+	path_grid[start] = (0,None)
 	
-	for y in range(min_y, max_y + 1):
-		line = ''
-		for x in range(min_x, max_x + 1):
-			pt = (x,y)
-			if pt == (0,0):
-				line += 'X'
-			elif pt in grid:
-#				line += str(grid[pt][1]).zfill(2)
-				if grid[pt][0] == 0:
-					line += '.'
-				elif grid[pt][0] == 1:
-					line += '-'
+	while len(to_visit) > 0:
+		visiting = to_visit.pop(0)
+		doors = path_grid[visiting][0]
+		for d in dir.values():
+			check_door = (visiting[0]+d[0],visiting[1]+d[1])
+			next = (check_door[0]+d[0],check_door[1]+d[1])
+			if check_door in grid and grid[check_door] == 1:
+				if next in path_grid:
+					if path_grid[next][0] > doors + 1:
+						path_grid[next] = (doors + 1, visiting)
+						to_visit.append(next)
 				else:
-					line += '|'
-			else:
-#				line += '##'
-				line += '#'
-		out.write(line+'\n')
-		print(line)
+					path_grid[next] = (doors + 1, visiting)
+					to_visit.append(next)
+	return(path_grid.values())
 
 def main():
-	input = get_input('input.txt')
-	branch_gen([0,0],input, 1)
-	furthest_room = max(grid.items(), key=itemgetter(1))[1][1]
-	far_rooms = 0
-	for room in grid.values():
-		if room[0] == 0 and room[1] >= 1000:
-			far_rooms += 1
+	global grid
+	grid[(0,0)] = 0
+	build_branch(get_input('input.txt')[1:-1], (0,0))
+	distances = bfs((0,0))
+	max_doors = max(distances, key=itemgetter(0))[0]
+	far_doors = 0
+	for d in distances:
+		if d[0] > 999:
+			far_doors += 1
 	
-	print('furthest room is ',furthest_room,'doors away.')
-	print('there are',far_rooms,'rooms at least 1000 doors away.')
+	print('furthest room is',max_doors,'doors away.')
+	print('there are',far_doors,'at least 1000 doors away.')
+
 main()
